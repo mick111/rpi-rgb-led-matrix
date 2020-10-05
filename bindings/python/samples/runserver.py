58e7edd5e5b70be29a725eeca62046ee02f14974
Michael <michaelm11188@gmail.com>
2020-10-05 23:36:41 +0200

    100

--- bindings/python/samples/runserver.py
+++ bindings/python/samples/runserver.py
@@ -301,7 +301,7 @@ class ServerHandler(SocketServer.BaseRequestHandler):
                                  pixo[(2*x,2*y+1)] = pix[(x,y)]
                                  pixo[(2*x+1,2*y)] = pix[(x,y)]
                                  pixo[(2*x+1,2*y+1)] = pix[(x,y)]
-                        ims.extend([imo] * 10)
+                        ims.extend([imo] * 100)
 
                 self.server.server_runner.timeBeforeIdle = time.time() + (float(commands[2]) if len(commands) > 2 else 10)
                 self.server.server_runner.images = ims
