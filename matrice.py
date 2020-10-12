10453b51d2cfa467b47c440c1e8d5cb62691f597
Michael <michaelm11188@gmail.com>
2020-10-13 00:42:14 +0200

    Mise a jour de matrice

--- matrice.py
+++ matrice.py
@@ -8,7 +8,8 @@ from rgbmatrix import graphics, RGBMatrix, RGBMatrixOptions
 
 import time
 import threading
-import SocketServer
+from tcp_server import ThreadedTCPServer, ServerHandler
+
 from PIL import Image
 import glob
 import argparse
@@ -70,27 +71,34 @@ EVENTS = {
 # Main Matrice class
 class Matrice(object):
     def __init__(self, args):
+        self.args = args
+
         # Create RGBMatrixOptions to launch arguments
         options = RGBMatrixOptions()
-        if args.led_gpio_mapping != None:
-            options.hardware_mapping = args.led_gpio_mapping
-        options.rows = args.led_rows
-        options.cols = args.led_cols
-        options.chain_length = args.led_chain
-        options.parallel = args.led_parallel
-        options.row_address_type = args.led_row_addr_type
-        options.multiplexing = args.led_multiplexing
-        options.pwm_bits = args.led_pwm_bits
-        options.brightness = args.led_brightness
-        options.pwm_lsb_nanoseconds = args.led_pwm_lsb_nanoseconds
-        options.led_rgb_sequence = args.led_rgb_sequence
-        options.pixel_mapper_config = args.led_pixel_mapper
-        if args.led_show_refresh:
+
+        if self.args.led_gpio_mapping is not None:
+            options.hardware_mapping = self.args.led_gpio_mapping
+        options.rows = self.args.led_rows
+        options.cols = self.args.led_cols
+        options.chain_length = self.args.led_chain
+        options.parallel = self.args.led_parallel
+        options.row_address_type = self.args.led_row_addr_type
+        options.multiplexing = self.args.led_multiplexing
+        options.pwm_bits = self.args.led_pwm_bits
+        options.brightness = self.args.led_brightness
+        options.pwm_lsb_nanoseconds = self.args.led_pwm_lsb_nanoseconds
+        options.led_rgb_sequence = self.args.led_rgb_sequence
+        options.pixel_mapper_config = self.args.led_pixel_mapper
+        options.panel_type = self.args.led_panel_type
+
+        if self.args.led_show_refresh:
             options.show_refresh_rate = 1
-        if args.led_slowdown_gpio != None:
-            options.gpio_slowdown = args.led_slowdown_gpio
-        if args.led_no_hardware_pulse:
+
+        if self.args.led_slowdown_gpio is not None:
+            options.gpio_slowdown = self.args.led_slowdown_gpio
+        if self.args.led_no_hardware_pulse:
             options.disable_hardware_pulsing = True
+
         self.matrix = RGBMatrix(options=options)
 
         try:
@@ -163,7 +171,7 @@ class Matrice(object):
     def updateFromConfigFile(self):
         try:
             fileinfo = json.load(open(self.fileinformation))
-        except:
+        except Exception:
             fileinfo = {
                 "hue": 0.0,
                 "saturation": 100.0,
@@ -228,7 +236,6 @@ class Matrice(object):
             json.dump(fileinfo, open(self.fileinformation, "w+"))
         except Exception as e:
             print("updateToConfigFile error", e)
-            pass
 
     # Add a text line in history
     def addToHistory(self, data):
@@ -338,7 +345,7 @@ class Matrice(object):
 
         # Print Temperatures
         tempFormat = "{:2.0f}"
-        temp = Weather().meanTemps()
+        temp = self.meteo.meanTemps()
         if temp is not None:
             length = graphics.DrawText(
                 ca, f, inTempPos[0], inTempPos[1], co, tempFormat.format(temp)
@@ -354,11 +361,11 @@ class Matrice(object):
                 )
 
         # Print weather icon (origin is TopLeft, coordinates are flipped)
-        ico = Weather().icon(iconSize)
+        ico = self.meteo.icon(iconSize)
         if ico is not None:
             ca.SetImage(ico, iconPos[0], iconPos[1])
 
-        outTemp = Weather().outsideTemperature()
+        outTemp = self.meteo.outsideTemperature()
         if outTemp is not None:
             length = graphics.DrawText(
                 ca, f, outTempPos[0], outTempPos[1], co, tempFormat.format(outTemp)
@@ -545,131 +552,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # RGBMatrixOptions specific options
-    parser.add_argument(
-        "-R",
-        "--led-rotation",
-        action="store",
-        help="Sets the rotation of matrix. Allowed: 0, 90, 180, 270. Default: 0.",
-        choices=[0, 90, 180, 270],
-        default=0,
-        type=int,
-    )
-    parser.add_argument(
-        "-r",
-        "--led-rows",
-        action="store",
-        help="Display rows. 16 for 16x32, 32 for 32x32. Default: 32",
-        default=32,
-        type=int,
-    )
-    parser.add_argument(
-        "--led-cols",
-        action="store",
-        help="Panel columns. Typically 32 or 64. (Default: 32)",
-        default=32,
-        type=int,
-    )
-    parser.add_argument(
-        "-c",
-        "--led-chain",
-        action="store",
-        help="Daisy-chained boards. Default: 1.",
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        "-P",
-        "--led-parallel",
-        action="store",
-        help="For Plus-models or RPi2: parallel chains. 1..3. Default: 1",
-        default=1,
-        type=int,
-    )
-    parser.add_argument(
-        "-p",
-        "--led-pwm-bits",
-        action="store",
-        help="Bits used for PWM. Something between 1..11. Default: 11",
-        default=11,
-        type=int,
-    )
-    parser.add_argument(
-        "-b",
-        "--led-brightness",
-        action="store",
-        help="Sets brightness level. Default: 100. Range: 1..100",
-        default=100,
-        type=int,
-    )
-    parser.add_argument(
-        "-m",
-        "--led-gpio-mapping",
-        help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm",
-        choices=["regular", "adafruit-hat", "adafruit-hat-pwm"],
-        type=str,
-    )
-    parser.add_argument(
-        "--led-scan-mode",
-        action="store",
-        help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)",
-        default=1,
-        choices=range(2),
-        type=int,
-    )
-    parser.add_argument(
-        "--led-pwm-lsb-nanoseconds",
-        action="store",
-        help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130",
-        default=130,
-        type=int,
-    )
-    parser.add_argument(
-        "--led-show-refresh",
-        action="store_true",
-        help="Shows the current refresh rate of the LED panel",
-    )
-    parser.add_argument(
-        "--led-slowdown-gpio",
-        action="store",
-        help="Slow down writing to GPIO. Range: 1..100. Default: 1",
-        choices=range(3),
-        type=int,
-    )
-    parser.add_argument(
-        "--led-no-hardware-pulse",
-        action="store",
-        help="Don't use hardware pin-pulse generation",
-    )
-    parser.add_argument(
-        "--led-rgb-sequence",
-        action="store",
-        help="Switch if your matrix has led colors swapped. Default: RGB",
-        default="RGB",
-        type=str,
-    )
-    parser.add_argument(
-        "--led-pixel-mapper",
-        action="store",
-        help='Apply pixel mappers. e.g "Rotate:90"',
-        default="",
-        type=str,
-    )
-    parser.add_argument(
-        "--led-row-addr-type",
-        action="store",
-        help="0 = default; 1=AB-addressed panels",
-        default=0,
-        type=int,
-        choices=[0, 1],
-    )
-    parser.add_argument(
-        "--led-multiplexing",
-        action="store",
-        help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral (Default: 0)",
-        default=0,
-        type=int,
-        choices=[0, 1, 2, 3],
-    )
+    parser.add_argument("-r", "--led-rows", action="store", help="Display rows. 16 for 16x32, 32 for 32x32. Default: 16", default=16, type=int)
+    parser.add_argument("--led-cols", action="store", help="Panel columns. Typically 32 or 64. (Default: 32)", default=32, type=int)
+    parser.add_argument("-c", "--led-chain", action="store", help="Daisy-chained boards. Default: 2.", default=2, type=int)
+    parser.add_argument("-P", "--led-parallel", action="store", help="For Plus-models or RPi2: parallel chains. 1..3. Default: 1", default=1, type=int)
+    parser.add_argument("-p", "--led-pwm-bits", action="store", help="Bits used for PWM. Something between 1..11. Default: 11", default=11, type=int)
+    parser.add_argument("-b", "--led-brightness", action="store", help="Sets brightness level. Default: 100. Range: 1..100", default=100, type=int)
+    parser.add_argument("-m", "--led-gpio-mapping", help="Hardware Mapping: regular, adafruit-hat, adafruit-hat-pwm, mick111" , choices=['regular', 'adafruit-hat', 'adafruit-hat-pwm', 'mick111'], type=str)
+    parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
+    parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
+    parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
+    parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 1", default=1, type=int)
+    parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
+    parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
+    parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="Rotate:180", type=str)
+    parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1=AB-addressed panels; 2=row direct; 3=ABC-addressed panels; 4 = ABC Shift + DE direct", default=0, type=int, choices=[0,1,2,3,4])
+    parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan; 8=ZStripeUneven... (Default: 0)", default=0, type=int)
+    parser.add_argument("--led-panel-type", action="store", help="Needed to initialize special panels. Supported: 'FM6126A'", default="", type=str)
 
     # Matrice specific options
     parser.add_argument(
